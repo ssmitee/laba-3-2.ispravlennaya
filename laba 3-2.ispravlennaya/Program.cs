bfcf787bf10eb714aa55313c7f3e76e4742d6d1b
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-28 22:13:17 +0200

    Добавьте файлы проекта.

---
+++ laba 3-2.ispravlennaya/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+
+int result = 0;
+
+while (n >= m)
+{
+    n = n - m;
+    result = result + 1;
+}
+
+result = result * result;
+
+Console.WriteLine("Результат квадрата целочисленного деления n на m: " + result);
